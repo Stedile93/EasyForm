fdd1ff22828d2254added3fe06a204877560915e
Giuliano Stedile <giu.stedile@gmail.com>
2015-04-02 09:47:35 -0300

    Estrutura do projeto e instalação do gulp e: jshint, rename, uglify, jasmine

---
+++ Gulpfile.js
@@ -0,0 +1,43 @@
+
+// Charge gulp and gulp plugins
+var gulp    = require('gulp');
+var jshint  = require('gulp-jshint');
+var uglify  = require('gulp-uglify');
+var rename  = require('gulp-rename');
+var jasmine = require('gulp-jasmine');
+
+// Define source folder
+var files = "./src/*.js";
+
+
+gulp.task('lint', function(){
+
+  // Code validation
+  gulp.src(files)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+
+
+});
+
+
+// Dist task
+gulp.task('dist', function(){
+
+  gulp.src(files)
+    .pipe(rename('easyform.jquery.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
+
+});
+
+
+// Default task
+gulp.task('default', function(){
+
+  gulp.run('lint', 'dist');
+  gulp.watch(files, function(evt){
+    gulp.run('lint', 'dist');
+  });
+
+});
